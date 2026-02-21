8dccacd53dc501db4d1dddb10658925b19138709
SlawekNowy <sosw.slawomir.spiewak@gmail.com>
2026-02-21 18:05:07 +0100

    More convincing ruse.

--- libs/ui.lua
+++ libs/ui.lua
@@ -7,12 +7,20 @@ SMODS.Atlas({
 
 logo = "balatro.png"
 
-SMODS.Atlas {
+SMODS.Atlas({
 		key = "balatro",
 		path = logo,
 		px = 333,
 		py = 216,
 		prefix_config = { key = false }
-	}
+	})
+
+
+G.FUNCS.fake_win_button = function() 
+	-- oh you you actually clicked something? then i'll laugh even harder!
+	G.showlaughingcat = 548
+	--G.FUNCS.exit_overlay_menu()
+end
+
 
 
